ebd8269798bbd741ad89112ce69334db9545d9ac
JValbjorn <julievalbjorn@live.dk>
2025-12-09 20:42:26 +0100

    login css

---
+++ public/icons/ArrowLogin.jsx
@@ -0,0 +1,7 @@
+    export default function ArrowLogin({ className = "" }) {
+  return (
+    <svg className={className} viewBox="0 0 20 12" width="20" height="12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0.847278 5.93091L18.9224 5.93091M18.9224 5.93091L13.8387 11.0145M18.9224 5.93091L13.8387 0.84729" stroke="#3B82F6" strokeWidth="1.69454" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
--- public/icons/CrossIcon.jsx
+++ public/icons/CrossIcon.jsx
@@ -1,7 +1,7 @@
 export default function CrossIcon() {
     return (
         <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M2 27L27 2M27 27L2 2" stroke="#3B82F6" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M2 27L27 2M27 27L2 2" stroke="#3B82F6" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 
     )}
