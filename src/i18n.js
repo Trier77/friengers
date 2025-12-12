937ea148953bf32e14537494997e6eb353f6cca6
JValbjorn <julievalbjorn@live.dk>
2025-12-12 12:46:45 +0100

    language toggle in setting

--- src/i18n.js
+++ src/i18n.js
@@ -1,19 +1,20 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import HttpBackend from "i18next-http-backend";
 
 i18n
   .use(HttpBackend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    lng: "en",
-    fallbackLng: "en",
+    fallbackLng: "da",
     interpolation: {
       escapeValue: false,
     },
     backend: {
       loadPath: "/locales/{{lng}}/translation.json",
-    }
+    },
   });
 
 export default i18n;
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -7,15 +7,23 @@ import DarkModeIcon from "../../public/icons/DarkModeIcon";
 import InfoIcon from "../../public/icons/InfoIcon";
 import HelpIcon from "../../public/icons/HelpIcon";
 import LogOutIcon from "../../public/icons/LogOutIcon";
+import LangIcon from "../../public/icons/Lang";
 import Toggle from "../components/Toggle";
 import { useState, useEffect } from "react";
 import { useContext } from "react";
 import { SettingsContext } from "../context/SettingsContext";
 import { logout } from "../auth";
+import { useTranslation } from "react-i18next";
+
 
 
 export default function Settings() {
   const navigate = useNavigate();
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lang) => {
+  i18n.changeLanguage(lang);
+};
 
   const { darkMode, setDarkMode, notifications, setNotifications } =
     useContext(SettingsContext);
@@ -38,19 +46,40 @@ export default function Settings() {
       </div>
 
       <div className="flex flex-col items-center gap-5 m-4">
-        <h1 className="text-(--primary) text-5xl">Indstillinger</h1>
+        <h1 className="text-(--primary) text-5xl">{t("settings")}</h1>
 
         <div>
           <div className="flex gap-4 items-center mb-4">
             <ProfileIcon />
-            <h2 className="text-(--primary) text-3xl ">Konto</h2>
+            <h2 className="text-(--primary) text-3xl ">{t("account")}</h2>
           </div>
+
+          <div className="flex gap-4 items-center mb-4">
+  <LangIcon />
+  <h2 className="text-(--primary) text-3xl ">{t("language")}</h2>
+
+  <div className="flex gap-2 ml-auto">
+    <button 
+      className=" py-1 rounded text-2xl"
+      onClick={() => changeLanguage("da")}
+    >
+      <img src="img/dannebrog.png" alt="Dannebrog" className="rounded-3xl h-7 w-7" />
+    </button>
+    <button 
+      className=" py-1 rounded text-2xl"
+      onClick={() => changeLanguage("en")}
+    >
+      <img src="img/union-jack.png" alt="Union Jack" className="rounded-3xl h-7 w-7" />
+    </button>
+  </div>
+</div>
+
 
           {/* Notifications */}
           <div className="flex justify-between items-center mb-4 gap-4">
             <div className="flex gap-4 items-center">
               <NotificationIcon color="--primary" size={20} />
-              <h2 className="text-(--primary) text-3xl">Notifikation</h2>
+              <h2 className="text-(--primary) text-3xl">{t("notifications")}</h2>
             </div>
 
             <Toggle enabled={notifications} setEnabled={setNotifications} />
@@ -60,7 +89,7 @@ export default function Settings() {
           <div className="flex justify-between items-center mb-4">
             <div className="flex gap-4 items-center">
               <DarkModeIcon />
-              <h2 className="text-(--primary) text-3xl">Dark mode</h2>
+              <h2 className="text-(--primary) text-3xl">{t("darkmode")}</h2>
             </div>
 
             <Toggle enabled={darkMode} setEnabled={setDarkMode} />
@@ -69,12 +98,12 @@ export default function Settings() {
 
         <div className="flex gap-4 items-center mb-4">
           <InfoIcon />
-          <h2 className="text-(--primary) text-3xl">Om os</h2>
+          <h2 className="text-(--primary) text-3xl">{t("about")}</h2>
         </div>
 
         <div className="flex gap-4 items-center mb-4">
           <HelpIcon />
-          <h2 className="text-(--primary) text-3xl">Hjælp og support</h2>
+          <h2 className="text-(--primary) text-3xl">{t("help")}</h2>
         </div>
 
         <div className="flex gap-4 items-center mb-4">
@@ -83,7 +112,7 @@ export default function Settings() {
             className="rounded text-(--primary) text-3xl"
             onClick={handleLogout}
           >
-            Log out
+            {t("logout")}
           </button>
           </div>
         </div>
