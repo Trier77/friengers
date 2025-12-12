e05f19f8330267dbc96611c8e9851e930affb4e4
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-12-12 09:08:43 +0100

    circle

--- src/components/ColorCircle.jsx
+++ src/components/ColorCircle.jsx
@@ -1,9 +1,9 @@
 export default function ColorCircle() {
   return (
     <div
-      className="absolute -top-100 left-1/2 -translate-x-1/2 -translate-y-1/2
-       w-300 h-300 rounded-full opacity-30 z-0 pointer-events-none
-       shadow-[inset_0_0_100px_rgba(59,130,246,0.4)]"
-    ></div>
+      className="oval-spin absolute -top-100 left-1/2 
+      w-300 h-300 rounded-full opacity-30 z-0 pointer-events-none
+      shadow-[inset_0_0_100px_rgba(59,130,246,0.4)]"
+    />
   );
 }
